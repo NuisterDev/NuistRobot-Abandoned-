353d3892e2f38ec6309f87435fec1b3e9a4bdce8
charlotte-xiao <charlottexiao123@gmail.com>
2021-03-05 16:59:54 +0800

    first request

--- src/test/kotlin/RunMirai.kt
+++ src/test/kotlin/RunMirai.kt
@@ -5,14 +5,17 @@ import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 
+@ConsoleExperimentalApi
 suspend fun main() {
+
     MiraiConsoleTerminalLoader.startAsDaemon()
 
     PluginMain.load()
     PluginMain.enable()
 
-    val bot = MiraiConsole.addBot(123456, "") {
+    MiraiConsole.addBot(2919025603, "NUISTrobot@") {
         fileBasedDeviceInfo()
     }.alsoLogin()
 
